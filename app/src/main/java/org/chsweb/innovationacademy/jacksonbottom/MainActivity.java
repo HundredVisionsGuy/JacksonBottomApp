41ac71bb3aa768092a5cfa608afb8081f37efcdb
Wadiepie <haileyparmentier33@gmail.com>
2017-05-01 11:39:16 -0700

    adding rules list view
    # Conflicts:
    #	.idea/modules.xml

--- app/src/main/java/org/chsweb/innovationacademy/jacksonbottom/MainActivity.java
+++ app/src/main/java/org/chsweb/innovationacademy/jacksonbottom/MainActivity.java
@@ -3,11 +3,14 @@ package org.chsweb.innovationacademy.jacksonbottom;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.location.LocationManager;
 import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView rulesView = (ListView) findViewById(R.id.rules_list_view);
+        Resources res = getResources();
+        String[] rules = res.getStringArray(R.array.Rules);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, rules);
+
+        rulesView.setAdapter(adapter);
+
+
         final Context context = this;
         LocationManager lm = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
         boolean gps_enabled = false;
