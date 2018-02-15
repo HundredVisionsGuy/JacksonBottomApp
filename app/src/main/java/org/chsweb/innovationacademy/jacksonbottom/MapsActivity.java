f14fafa117d7dad534ebaa0c2d06370351b6928b
Theodore Harris <bananyog@gmail.com>
2018-02-14 16:12:01 -0800

    Added multiple markers, changed default API from 25.0.0 to 26.0.2

--- app/src/main/java/org/chsweb/innovationacademy/jacksonbottom/MapsActivity.java
+++ app/src/main/java/org/chsweb/innovationacademy/jacksonbottom/MapsActivity.java
@@ -48,6 +48,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             LatLng jackson_bottom = new LatLng(45.5007, -122.9903);
             LatLng upland_ponds = new LatLng(45.50023, -122.9895);
             LatLng mason_bee_boxes = new LatLng(45.500105, -122.98923);
+            LatLng east_side_trail = new LatLng(45.50075, -122.9894);
+            LatLng intersection = new LatLng(45.50082, -122.9894);
+
             mMap.addMarker(new MarkerOptions().position(jackson_bottom).
                     title(getString(R.string.marker_jackson_main)).
                     snippet("Hey, I'm a snippet. Yo!"));
@@ -57,6 +60,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.addMarker(new MarkerOptions().position(mason_bee_boxes).
                     title(getString(R.string.marker_bee_boxes)).
                     snippet(getString(R.string.snippet_bee_boxes)));
+            mMap.addMarker(new MarkerOptions().position(east_side_trail).
+                    title(getString(R.string.east_side_trail)).
+                    snippet(getString(R.string.snippet_east_side)));
+            mMap.addMarker(new MarkerOptions().position(intersection).
+                    title(getString(R.string.intersection)).
+                    snippet(getString(R.string.snippet_intersection)));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(jackson_bottom));
         } catch (Exception e) {
             Context context = getApplicationContext();
