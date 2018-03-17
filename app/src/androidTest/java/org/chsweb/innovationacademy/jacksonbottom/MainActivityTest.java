f127384f162bf2e85cf502ff7411ae5b47926b6d
HundredVisionsGuy <winikkc@hsd.k12.or.us>
2018-03-16 17:41:27 -0700

    Adding UITest dependencies and LaunchMaps testing

---
+++ app/src/androidTest/java/org/chsweb/innovationacademy/jacksonbottom/MainActivityTest.java
@@ -0,0 +1,53 @@
+package org.chsweb.innovationacademy.jacksonbottom;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+/**
+ * Created by winikkc on 3/16/18.
+ */
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    private MainActivity mainActivity = null;
+    
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(MapsActivity.class.getName(), null, false);
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = mainActivityActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunchMapActivityOnButtonClick() {
+        assertNotNull(mainActivity.findViewById(R.id.button_launch_map_activity));
+
+        onView(withId(R.id.button_launch_map_activity)).perform(click());
+
+        Activity mapsActivity = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+
+        assertNotNull(mapsActivity);
+
+        mapsActivity.finish();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mainActivity = null;
+    }
+
+}
