ae7773ae7a415433bf7a62ebecd118929720e0ad
hundredvisionsguy <winikkc@hsd.k12.or.us>
2018-04-11 17:14:43 -0700

    Merge branch 'master' of https://github.com/HundredVisionsGuy/JacksonBottomApp
    # Conflicts:
    #	app/src/main/java/org/chsweb/innovationacademy/jacksonbottom/VirtualTourActivity.java

--- app/src/main/java/org/chsweb/innovationacademy/jacksonbottom/AppUtils.java
+++ app/src/main/java/org/chsweb/innovationacademy/jacksonbottom/AppUtils.java
@@ -2,7 +2,6 @@ package org.chsweb.innovationacademy.jacksonbottom;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 
 /**
